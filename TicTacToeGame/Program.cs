159af00d3ce12f2462f67a835bb50219f019b26b
Kmrunal20 <mrunal.kurhade@digiplusit.com>
2020-10-10 15:15:00 +0530

    [mrunal] Add. Added a feature to create tictactoe game board

--- TicTacToeGame/Program.cs
+++ TicTacToeGame/Program.cs
@@ -4,9 +4,11 @@ namespace TicTacToeGame
 {
     class Program
     {
+        public static TicTacToeGame ticTacToe = new TicTacToeGame();
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to TicTacToe Game !");
+            ticTacToe.CreateGameBoard();
         }
     }
 }
